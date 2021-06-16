c5f6abf90d35544d8b756852e34c66f4791f5583
Nazik Beksyltanova <bekntemirova@gmail.com>
2021-06-16 21:19:44 +0600

    практиковала переменные jS

---
+++ js/index.js
@@ -0,0 +1,62 @@
+/*alert("heloo");
+alert("hello worlt");
+alert(3 + 1 + 2);*/
+
+//alert("Error");
+//[(1, 2)].forEach(alert);
+
+//let message;
+
+//message = "hello bro";
+
+//alert(massage);
+
+//var message = "hrkii";
+
+/*let user = " jom";
+let age = 40;
+alert(user);
+alert(age);*/
+//const interestRate = 2;
+//alert(interestRate);
+/*let userName = "Nazik";
+let ago = 18;
+alert(userName);
+alert(ago);*/
+
+/*let a = 100;
+let b = 200;
+let temp = a;
+
+a = b;
+b = temp;
+alert(" значенние a: " + a);
+alert(" значенние b:" + b);
+//alert(temp);*/
+
+/*let a = 300;
+let b = 600;
+let pere = a;
+
+a = b;
+b = pere;
+
+alert("ksk:" + a);
+alert("poj:" + b);
+//alert("pere");*/
+
+//Home work 1
+
+let admin = "Ivan";
+
+let name = "Ivan";
+
+alert("Ivan");
+
+//home work
+
+let ourPlanetName = "Earth";
+let currentName = "Alisa";
+
+alert(" Earth");
+alert("Alisa");
